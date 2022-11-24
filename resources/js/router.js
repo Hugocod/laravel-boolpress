ef2e5ded219ca876d87e0ad6d8061c4805c2d29b
Hugocod <u.medioli@icloud.com>
2022-11-24 09:15:20 +0100

    added post details in vue

--- resources/js/router.js
+++ resources/js/router.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 
 import PostComponent from "./components/PostComponent";
 import HomeComponent from "./components/pages/HomeComponent";
+import PostsIndexComponent from "./components/PostsIndexComponent";
 import AboutComponent from "./components/pages/AboutComponent";
 import NotFoundComponent from "./components/pages/NotFoundComponent";
 
@@ -11,6 +12,7 @@ Vue.use(VueRouter);
 const router = new VueRouter({
     routes: [
         { path: "/", name: "home", component: HomeComponent },
+        { path: "/posts", name: "posts-index", component: PostsIndexComponent },
         { path: "/pages/posts", name: "posts", component: PostComponent },
         { path: "/pages/about", name: "about", component: AboutComponent },
         { path: "/*", name: "NotFound", component: NotFoundComponent },
